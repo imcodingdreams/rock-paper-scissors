6912467d3d0bf4ed6599a98436c4936b5807b128
imcodingdreams <naykavallejo@yahoo.com>
2022-08-26 21:35:16 -0700

    Add choices comparison

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 
-
-// for (let i = 0; i < images.length; i++) {
-//   const choice = images[i];
-// }
-
-
+// Users's Choice
 
 const rockChoice = document.getElementById("rock");
 const paperChoice = document.getElementById("paper");
@@ -14,43 +9,25 @@ rockChoice.addEventListener("click", functionRock);
 paperChoice.addEventListener("click", functionPaper);
 scissorsChoice.addEventListener("click", functionScissors);
 
-
 function functionRock() {
   let text = rockChoice;
-  userChoice.innerHTML = "The user chose rock";
+  userChoice.innerText = "The user chose rock";
 }
 
 function functionPaper() {
   let text = paperChoice;
-  userChoice.innerHTML = "The user chose paper";
+  userChoice.innerText = "The user chose paper";
 }
 
 function functionScissors() {
   let text = scissorsChoice;
-  userChoice.innerHTML = "The user chose scissors";
+  userChoice.innerText = "The user chose scissors";
 }
 
-// const comp = document.getElementById("compChoice");
 
-
-// compChoice.addEventListener("click", compRdmChoice);
-// let index = compRdmChoice(0, compOptions.length);
-
-// function compRdmChoice() {
-//   if (compChoice = rockChoice) {
-//     userChoice.innerHTML = "The user chose rock";
-//   } else if (compChoice = paperChoice) {
-//     userChoice.innerHTML = "The user chose paper";
-//   } else {
-//     userChoice.innerHTML = "The user chose scissors";
-//   }
-// }
-
-// compRdmChoice();
+// Computer's Random Choice
 
 let result = document.getElementById("compChoice");
-
-
 
 let compOptions = [
   "The computer chose rock",
@@ -62,7 +39,7 @@ function compRdmChoice(min, max) {
   let step1 = max - min + 1;
   let step2 = Math.random() * step1;
   let result = Math.floor(step2) + min;
-  
+
   return result;
 }
 
@@ -71,29 +48,48 @@ rdmChoice.addEventListener("click", () => {
   result.innerText = compOptions[index];
 });
 
+// Choices Comparison
+
+// How can I grab the score results and put them in the usersChoice and compChoice variables?
+
+let choice1 = userChoice; 
+let choice2 = compChoice;
+let score = document.getElementById("gameResults");
+
+function scoreResults() {
+  if (choice1 === "rock" && choice2 === "scissors") {
+    if (choice1 === "paper" && choice2 === "rock") {
+      if (choice1 === "scissors" && choice2 === "paper") {
+        return (score.innerText = "The user won!");
+      }
+    } else {
+      if (choice1 === "rock" && choice2 === "paper") {
+        if (choice1 === "paper" && choice2 === "scissors") {
+          if (choice1 === "scissors" && choice2 === "rock") {
+            return (score.innerText = "The computer won!");
+          }
+        } else {
+          if (choice1 === "rock" && choice2 === "rock") {
+            if (choice1 === "paper" && choice2 === "paper") {
+              if (choice1 === "scissors" && choice2 === "scissors") {
+                return (score.innerText = "It's a tie!");
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+scoreResults();
+
+
 
 // let index = compOptions
 
-
-
-
+// for (let i = 0; i < images.length; i++)
+//   const choice = images[i];
+// }
 
 // How can I make the computer knows that she have to pick a random choice after the usersChoice?
-
-// function getRdmInteger() {
-//   let text = document.getElementById().innerText;
-//   compChoice.innerHTML = "The computer chose rock";
-// }
-
-// getRdmInteger();
-
-// function functionPaper() {
-//   let text = document.getElementById("paper").innerText;
-//   computerChoice.innerHTML = "The computer chose paper";
-// }
-
-// function functionScissors() {
-//   let text = document.getElementById("scissors").innerText;
-//   computerChoice.innerHTML = "The computer chose scissors";
-// }
-
