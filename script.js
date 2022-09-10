1f591c2e81ab2d21cfdb69f1d8679b20e2c4dc9b
imcodingdreams <naykavallejo@yahoo.com>
2022-09-10 15:31:09 -0700

    Add event listener, DOM manipulation and  user and Computer Choice comparison

--- script.js
+++ script.js
@@ -1,95 +1,43 @@
+const choices = document.querySelectorAll(".choices");
 
-// Users's Choice
+choices.forEach((choices) => {
+  choices.addEventListener("click", function () {
+    const userChoice = document.getElementById("userChoice");
+    document.getElementById("userChoice").innerText = `User chose ${userChoice}`;
 
-const rockChoice = document.getElementById("rock");
-const paperChoice = document.getElementById("paper");
-const scissorsChoice = document.getElementById("scissors");
-
-rockChoice.addEventListener("click", functionRock);
-paperChoice.addEventListener("click", functionPaper);
-scissorsChoice.addEventListener("click", functionScissors);
-
-function functionRock() {
-  let text = rockChoice;
-  userChoice.innerText = "The user chose rock";
-}
-
-function functionPaper() {
-  let text = paperChoice;
-  userChoice.innerText = "The user chose paper";
-}
-
-function functionScissors() {
-  let text = scissorsChoice;
-  userChoice.innerText = "The user chose scissors";
-}
-
-
-// Computer's Random Choice
-
-let result = document.getElementById("compChoice");
-
-let compOptions = [
-  "The computer chose rock",
-  "The computer chose paper",
-  "The computer chose scissors",
-];
-
-function compRdmChoice(min, max) {
-  let step1 = max - min + 1;
-  let step2 = Math.random() * step1;
-  let result = Math.floor(step2) + min;
-
-  return result;
-}
-
-rdmChoice.addEventListener("click", () => {
-  let index = compRdmChoice(0, compOptions.length - 1);
-  result.innerText = compOptions[index];
+    const compOptions = ["rock", "paper", "scissors"];
+    const compChoice = compOptions[Math.floor(Math.random() * 3)];
+    document.getElementById("compChoice").innerText = `Computer chose ${compChoice}`;
+  });
 });
 
-// Choices Comparison
+// const currentUserChoice = `User chose ${userChoice}`;
+// const currentCompChoice = `Computer chose ${compChoice}`;
 
-// How can I grab the score results and put them in the usersChoice and compChoice variables?
-
-let choice1 = userChoice; 
-let choice2 = compChoice;
-let score = document.getElementById("gameResults");
-
-function scoreResults() {
-  if (choice1 === "rock" && choice2 === "scissors") {
-    if (choice1 === "paper" && choice2 === "rock") {
-      if (choice1 === "scissors" && choice2 === "paper") {
-        return (score.innerText = "The user won!");
-      }
+//  Comparison
+const compareChoices = (userChoice, compChoice) => {
+  // Tie
+  if (userChoice === compChoice) {
+    return gameResults.innerText = "It's a tie!";
+  }
+  // Rock
+  if (userChoice === "rock") {
+    if (compChoice === "scissors") {
+      return gameResults.innerText = "You won!";
     } else {
-      if (choice1 === "rock" && choice2 === "paper") {
-        if (choice1 === "paper" && choice2 === "scissors") {
-          if (choice1 === "scissors" && choice2 === "rock") {
-            return (score.innerText = "The computer won!");
-          }
-        } else {
-          if (choice1 === "rock" && choice2 === "rock") {
-            if (choice1 === "paper" && choice2 === "paper") {
-              if (choice1 === "scissors" && choice2 === "scissors") {
-                return (score.innerText = "It's a tie!");
-              }
-            }
-          }
-        }
-      }
+      return gameResults.innerText = "The computer won!";
     }
   }
-}
-
-scoreResults();
-
-
-
-// let index = compOptions
-
-// for (let i = 0; i < images.length; i++)
-//   const choice = images[i];
-// }
-
-// How can I make the computer knows that she have to pick a random choice after the usersChoice?
+};
+  if (userChoice === "paper" && compChoice === "rock") {
+    gameResults.innerText = "You won!";
+  } else {
+    return gameResults.innerText = "The computer won!";
+  }
+  // Scissors
+  if (userChoice === "scissors" && compChoice === "paper") {
+    gameResults.innerText = "You won!";
+  } else {
+    return gameResults.innerText = "The computer won!";
+  }
+};
